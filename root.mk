2b614db273ba4717463395e031178bee8c1677db
Dong Xiang <dong.xiang@unisoc.com>
2018-10-30 17:01:03 +0800

    Add default target mp_test.
    Add default target mp_test.
    
    Signed-off-by: Dong Xiang <dong.xiang@unisoc.com>

--- root.mk
+++ root.mk
@@ -7,6 +7,8 @@
 BOARD	?= 96b_ivy5661
 BOOT	:= mcuboot
 KERNEL	:= zephyr
+REPEATER	:= repeater
+MP_TEST		:= mp_test
 
 # Directories and Files
 ################################################################
@@ -20,14 +22,17 @@ kernel_DIR	:= $(PRJDIR)/$(KERNEL)
 
 BUILD_DIR		:= $(PRJDIR)/output
 boot_BUILD_DIR		:= $(BUILD_DIR)/$(BOOT)
-kernel_BUILD_DIR	:= $(BUILD_DIR)/$(KERNEL)
+repeater_BUILD_DIR	:= $(BUILD_DIR)/$(REPEATER)
+mp_test_BUILD_DIR	:= $(BUILD_DIR)/$(MP_TEST)
 
 BOOT_BIN	:= $(boot_BUILD_DIR)/$(KERNEL)/$(KERNEL).bin
-KERNEL_BIN	:= $(kernel_BUILD_DIR)/$(KERNEL)/$(KERNEL).bin
+REPEATER_BIN	:= $(repeater_BUILD_DIR)/$(KERNEL)/$(KERNEL).bin
+MP_TEST_BIN	:= $(mp_test_BUILD_DIR)/$(KERNEL)/$(KERNEL).bin
 
 DIST_DIR	:= $(PRJDIR)/output/images
 BOOT_DIST_BIN	:= $(DIST_DIR)/$(BOOT)-pubkey.bin
-KERNEL_DIST_BIN	:= $(DIST_DIR)/$(KERNEL)-signed-ota.bin
+REPEATER_DIST_BIN	:= $(DIST_DIR)/$(REPEATER)-signed-ota.bin
+MP_TEST_DIST_BIN	:= $(DIST_DIR)/$(MP_TEST)-signed-ota.bin
 
 IMGTOOL = $(boot_DIR)/scripts/imgtool.py
 
@@ -82,7 +87,7 @@ endef
 
 # Targets
 ################################################################
-DEFAULT_TARGETS		:= boot kernel
+DEFAULT_TARGETS		:= boot repeater mp_test
 DIST_TARGETS		:= $(DEFAULT_TARGETS) 
 ALL_TARGETS		:= $(DEFAULT_TARGETS)
 CLEAN_TARGETS		:= $(addsuffix -clean,$(ALL_TARGETS))
@@ -91,7 +96,8 @@ CLEAN_TARGETS		:= $(addsuffix -clean,$(ALL_TARGETS))
 dist: $(DIST_TARGETS)
 	@ if [ ! -d $(DIST_DIR) ]; then install -d $(DIST_DIR); fi
 	@ install $(BOOT_BIN) $(BOOT_DIST_BIN)
-	$(call SIGN_KERNEL_IMAGE,$(KERNEL_BIN),$(KERNEL_DIST_BIN))
+	$(call SIGN_KERNEL_IMAGE,$(REPEATER_BIN),$(REPEATER_DIST_BIN))
+	$(call SIGN_KERNEL_IMAGE,$(MP_TEST_BIN),$(MP_TEST_DIST_BIN))
 
 .PHONY: clean
 clean: $(CLEAN_TARGETS)
@@ -106,7 +112,9 @@ distclean:
 # Build Targets
 $(eval $(call MAKE_TARGET,boot,$(boot_DIR)/boot/zephyr))
 
-$(eval $(call MAKE_TARGET,kernel,$(app_DIR)/repeater))
+$(eval $(call MAKE_TARGET,repeater,$(app_DIR)/repeater))
+
+$(eval $(call MAKE_TARGET,mp_test,$(app_DIR)/mp_test))
 
 # Clean Targets
 $(foreach target,$(ALL_TARGETS),$(eval $(call CLEAN_TARGET,$(target),clean)))
